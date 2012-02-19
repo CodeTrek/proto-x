1ff47231d7ac27a75791ed0312365985d7898553
code.trek.1 <code.trek.1@b8e2dd6e-3391-11de-8fef-1f5ae417a2df>
2012-02-19 03:10:33 +0000

    unit test refactoring

--- proto-x/test/protox/custom/custom.cpp
+++ proto-x/test/protox/custom/custom.cpp
@@ -5,14 +5,14 @@
     or http://www.opensource.org/licenses/mit-license.php)
 */
 
-/******************************************************************************/
+/**********************************************************************************************************************/
 
 #define BOOST_AUTO_TEST_MAIN
 
 #include <boost/test/included/unit_test_framework.hpp>
 #include <boost/test/auto_unit_test.hpp>
 
-/******************************************************************************/
+/**********************************************************************************************************************/
 
 #include <protox/platform.hpp>
 
@@ -25,86 +25,78 @@
 
 #include <test/protox/custom/custom_data_representation_table.hpp>
 
-/******************************************************************************/
+/**********************************************************************************************************************/
 
-  namespace t1
-  {
+namespace t1 {
     using namespace protox::dtl;
     using namespace test::custom;
 
-    struct SimpleUnsigned16BE : simple< UnsignedInt16BE >
-      { PROTOX_SIMPLE( SimpleUnsigned16BE ) }; 
-  }
+    struct SimpleUnsigned16BE : simple< UnsignedInt16BE > { PROTOX_SIMPLE( SimpleUnsigned16BE ) };
+}
 
-  BOOST_AUTO_TEST_CASE( test_encode_UnsignedInt16BE )
-  {
+/**********************************************************************************************************************/
+
+BOOST_AUTO_TEST_CASE(test_encode_UnsignedInt16BE) {
     using namespace t1;
     protox::io::byte_data_sink sink;
 
     SimpleUnsigned16BE uint16BE = 23171;
-    sink.encode( uint16BE );
+    sink.encode(uint16BE);
 
-    BOOST_CHECK( sink.size() == 2 );
-    BOOST_CHECK(
-      ntohs( *((SimpleUnsigned16BE *) sink.getDataBuffer()) ) == 23171 );
-  }
+    BOOST_CHECK(sink.size() == 2);
+    BOOST_CHECK(ntohs(*((SimpleUnsigned16BE *) sink.getDataBuffer())) == 23171);
+}
 
-/******************************************************************************/
+/**********************************************************************************************************************/
 
-  namespace t2
-  {
+namespace t2 {
     using namespace protox::dtl;
     using namespace test::custom;
 
-    struct SimpleUnsigned32BE : simple< UnsignedInt32BE >
-      { PROTOX_SIMPLE( SimpleUnsigned32BE ) }; 
-  }
+    struct SimpleUnsigned32BE : simple< UnsignedInt32BE > { PROTOX_SIMPLE(SimpleUnsigned32BE) };
+}
 
-  BOOST_AUTO_TEST_CASE( test_encode_UnsignedInt32BE )
-  {
+/**********************************************************************************************************************/
+
+BOOST_AUTO_TEST_CASE(test_encode_UnsignedInt32BE) {
     using namespace t2;
     protox::io::byte_data_sink sink;
 
     SimpleUnsigned32BE uint32BE = 368974;
-    sink.encode( uint32BE );
+    sink.encode(uint32BE);
 
-    BOOST_CHECK( sink.size() == 4 );
+    BOOST_CHECK(sink.size() == 4);
 
-    SimpleUnsigned32BE value
-      = SYS_NTOHL( *((SimpleUnsigned32BE *) sink.getDataBuffer()) );
+    SimpleUnsigned32BE value = SYS_NTOHL(*((SimpleUnsigned32BE *) sink.getDataBuffer()));
 
-    BOOST_CHECK( value == 368974 );
-  }
+    BOOST_CHECK(value == 368974);
+}
 
-/******************************************************************************/
+/**********************************************************************************************************************/
 
-  namespace t3
-  {
+namespace t3 {
     using namespace protox::dtl;
     using namespace test::custom;
 
-    struct SimpleUnsigned32BE : simple< UnsignedInt32BE >
-      { PROTOX_SIMPLE( SimpleUnsigned32BE ) }; 
+    struct SimpleUnsigned32BE : simple< UnsignedInt32BE > { PROTOX_SIMPLE(SimpleUnsigned32BE) };
 
-    struct SimpleUnsigned64BE : simple< UnsignedInt64BE >
-      { PROTOX_SIMPLE( SimpleUnsigned64BE ) }; 
-  }
+    struct SimpleUnsigned64BE : simple< UnsignedInt64BE > { PROTOX_SIMPLE(SimpleUnsigned64BE) };
+}
 
-  BOOST_AUTO_TEST_CASE( test_encode_UnsignedInt64BE )
-  {
+/**********************************************************************************************************************/
+
+BOOST_AUTO_TEST_CASE( test_encode_UnsignedInt64BE ) {
     using namespace t3;
     protox::io::byte_data_sink sink;
 
     SimpleUnsigned64BE uint64BE = 3684294UL;
-    sink.encode( uint64BE );
+    sink.encode(uint64BE);
 
-    BOOST_CHECK( sink.size() == 8 );
+    BOOST_CHECK(sink.size() == 8);
 
-    SimpleUnsigned32BE lowBytes
-      = SYS_NTOHL( *((SimpleUnsigned32BE *) (sink.getDataBuffer() + 4)) );
+    SimpleUnsigned32BE lowBytes = SYS_NTOHL(*((SimpleUnsigned32BE *) (sink.getDataBuffer() + 4)));
 
-    SimpleUnsigned32BE highBytes
-      = SYS_NTOHL( *((SimpleUnsigned32BE *) sink.getDataBuffer()) );
+    SimpleUnsigned32BE highBytes = SYS_NTOHL(*((SimpleUnsigned32BE *) sink.getDataBuffer()));
 
     SimpleUnsigned64BE value = 0;
     char *vp = (char *) &value;
@@ -117,95 +109,91 @@
     vp[6] = sink.getDataBuffer()[1];
     vp[7] = sink.getDataBuffer()[0];
 
-    BOOST_CHECK( value == 3684294UL );
-  }
+    BOOST_CHECK(value == 3684294UL);
+}
 
-/******************************************************************************/
+/**********************************************************************************************************************/
 
-  namespace t4
-  {
+namespace t4 {
     using namespace protox::dtl;
     using namespace test::custom;
 
-    struct SimpleUnsigned16BE : simple< UnsignedInt16BE >
-      { PROTOX_SIMPLE( SimpleUnsigned16BE ) };
-  }
+    struct SimpleUnsigned16BE : simple< UnsignedInt16BE > { PROTOX_SIMPLE(SimpleUnsigned16BE) };
+}
 
-  BOOST_AUTO_TEST_CASE( test_decode_Unsigned16BE )
-  {
+/**********************************************************************************************************************/
+
+BOOST_AUTO_TEST_CASE(test_decode_Unsigned16BE) {
     using namespace t4;
     protox::io::byte_data_sink sink;
 
     SimpleUnsigned16BE uint16BE = 23171;
-    sink.encode( uint16BE );
+    sink.encode(uint16BE);
 
-    BOOST_CHECK( sink.size() == 2 );
-    BOOST_CHECK( ntohs( *((SimpleUnsigned16BE *) sink.getDataBuffer()) ) == 23171 );
+    BOOST_CHECK(sink.size() == 2);
+    BOOST_CHECK(ntohs(*((SimpleUnsigned16BE *) sink.getDataBuffer())) == 23171);
 
-    protox::io::byte_data_source source( sink );
+    protox::io::byte_data_source source(sink);
     SimpleUnsigned16BE uint16BE_d = 0;
-    source.decode( uint16BE_d );
+    source.decode(uint16BE_d);
 
-    BOOST_CHECK( uint16BE_d == 23171 );
-  }
+    BOOST_CHECK(uint16BE_d == 23171);
+}
 
-/******************************************************************************/
+/**********************************************************************************************************************/
 
-  namespace t5
-  {
+namespace t5 {
     using namespace protox::dtl;
     using namespace test::custom;
 
-    struct SimpleUnsigned32BE : simple< UnsignedInt32BE >
-      { PROTOX_SIMPLE( SimpleUnsigned32BE ) };
-  }
+    struct SimpleUnsigned32BE : simple< UnsignedInt32BE > { PROTOX_SIMPLE(SimpleUnsigned32BE) };
+ }
 
-  BOOST_AUTO_TEST_CASE( test_decode_HLAinteger32BE )
-  {
+/**********************************************************************************************************************/
+
+BOOST_AUTO_TEST_CASE(test_decode_HLAinteger32BE) {
     using namespace t5;
     protox::io::byte_data_sink sink;
 
     SimpleUnsigned32BE uint32BE = 36894;
-    sink.encode( uint32BE );
+    sink.encode(uint32BE);
 
-    BOOST_CHECK( sink.size() == 4 );
+    BOOST_CHECK(sink.size() == 4);
 
-    SimpleUnsigned32BE value
-      = SYS_NTOHL( *((SimpleUnsigned32BE *) sink.getDataBuffer()) );
+    SimpleUnsigned32BE value = SYS_NTOHL(*((SimpleUnsigned32BE *) sink.getDataBuffer()));
 
-    BOOST_CHECK( value == 36894 );
+    BOOST_CHECK(value == 36894);
 
-    protox::io::byte_data_source source( sink );
+    protox::io::byte_data_source source(sink);
     SimpleUnsigned32BE uint32BE_d = 0;
-    source.decode( uint32BE_d );
+    source.decode(uint32BE_d);
 
-    BOOST_CHECK( uint32BE_d == 36894 );
-  }
+    BOOST_CHECK(uint32BE_d == 36894);
+}
 
-/******************************************************************************/
+/**********************************************************************************************************************/
 
-  namespace t6
-  {
+namespace t6 {
     using namespace protox::dtl;
     using namespace test::custom;
 
-    struct SimpleUnsigned64BE : simple< UnsignedInt64BE >
-      { PROTOX_SIMPLE( SimpleUnsigned64BE ) }; 
-  }
+    struct SimpleUnsigned64BE : simple< UnsignedInt64BE > { PROTOX_SIMPLE(SimpleUnsigned64BE) };
+}
 
-  BOOST_AUTO_TEST_CASE( test_decode_HLAinteger64BE )
-  {
+/**********************************************************************************************************************/
+
+BOOST_AUTO_TEST_CASE(test_decode_HLAinteger64BE) {
     using namespace t6;
     protox::io::byte_data_sink sink;
 
     SimpleUnsigned64BE uint64BE = 3386894UL;
-    sink.encode( uint64BE );
+    sink.encode(uint64BE);
 
-    BOOST_CHECK( sink.size() == 8 );
+    BOOST_CHECK(sink.size() == 8);
 
-    protox::io::byte_data_source source( sink );
+    protox::io::byte_data_source source(sink);
     SimpleUnsigned64BE uint64BE_d = 0;
-    source.decode( uint64BE_d );
+    source.decode(uint64BE_d);
 
-    BOOST_CHECK( uint64BE_d == 3386894UL );
-  }
+    BOOST_CHECK(uint64BE_d == 3386894UL);
+}
