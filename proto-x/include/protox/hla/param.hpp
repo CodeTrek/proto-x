9f5991e2dd687475e8286a3b3cbb58a2315fd65b
code.trek.1 <code.trek.1@b8e2dd6e-3391-11de-8fef-1f5ae417a2df>
2012-03-01 02:28:12 +0000

    unit test refactoring

--- proto-x/include/protox/hla/param.hpp
+++ proto-x/include/protox/hla/param.hpp
@@ -5,12 +5,12 @@
     or http://www.opensource.org/licenses/mit-license.php)
 */
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 #ifndef PROTOX_HLA_PARAM_HPP
 #define PROTOX_HLA_PARAM_HPP
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 #include <string>
 
@@ -22,60 +22,52 @@
 #include <protox/io/byte_data_sink.hpp>
 #include <protox/io/byte_data_source.hpp>
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 namespace protox {
 namespace hla {
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 template< typename T >
-struct param_base
-{
-  typedef T param_type;
-  typename T::value_type value;
+struct param_base {
+    typedef T param_type;
+    typename T::value_type value;
 
-  RTI::ParameterHandle handle;
+    RTI::ParameterHandle handle;
 
-  static char const *name() { return T::name(); }
+    static char const *name() { return T::name(); }
 
-  param_base() : handle(0) {}
+    param_base() : handle(0) {}
 
-protected:
-  template< typename S >
-  void init_handle(const std::string &class_name)
-  {
-    handle = S::get_param_handle( class_name, name() );
-  }
+    protected:
+        template< typename S >
+        void init_handle(const std::string &class_name) { handle = S::get_param_handle(class_name, name()); }
 
-  void add_value( boost::shared_ptr< RTI::ParameterHandleValuePairSet > set_ptr )
-  {
-    protox::io::byte_data_sink sink;
-    sink.encode( value );
-    set_ptr->add( handle, sink.getDataBuffer(), (unsigned long) sink.getDataBufferSize() );
-  }
+        void add_value(boost::shared_ptr< RTI::ParameterHandleValuePairSet > set_ptr) {
+            protox::io::byte_data_sink sink;
+            sink.encode(value);
+            set_ptr->add(handle, sink.getDataBuffer(), (unsigned long) sink.getDataBufferSize());
+        }
   
-  void recv_value( const RTI::ParameterHandleValuePairSet &ph_set )
-  {
-    for (RTI::ULong i = 0; i < ph_set.size(); ++i)
-    {
-      const RTI::Handle h = ph_set.getHandle( i );
-      
-      if (handle == h)
-      {
-        RTI::ULong length = 0;
-        char *buff = ph_set.getValuePointer( i, length );
+        void recv_value(const RTI::ParameterHandleValuePairSet &ph_set) {
+            for (RTI::ULong i = 0; i < ph_set.size(); ++i) {
+                const RTI::Handle h = ph_set.getHandle(i);
+
+                if (handle == h) {
+                    RTI::ULong length = 0;
+                    char *buff = ph_set.getValuePointer(i, length);
         
-        protox::io::byte_data_source source( buff, length );
-        source.decode( value );
+                    protox::io::byte_data_source source(buff, length);
+                    source.decode(value);
         
-        break;
-      }
-    }
-  }
+                    break;
+                }
+            }
+        }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 /**
  * Use this template to generate an parameter definition type that can be used as part of an
@@ -104,161 +96,117 @@ protected:
  *
  */
 template< typename T >
-struct param
-{
-  typedef T value_type;
+struct param {
+    typedef T value_type;
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 // Forward declaration
 template< typename A, typename B > struct param_inherit;
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
-struct param_empty_base
-{
-  static RTI::ULong count_params() { return 0; }
+struct param_empty_base {
+    static RTI::ULong count_params() { return 0; }
 
-  template< typename T >
-  void init_handles( const std::string &class_name ) {}
+    template< typename T >
+    void init_handles(const std::string &class_name) {}
 
-  void recv_values( const RTI::ParameterHandleValuePairSet & ) {}
+    void recv_values( const RTI::ParameterHandleValuePairSet & ) {}
 
-  template< typename T >
-  inline RTI::ParameterHandle get_param_handle()
-  {
-    throw RTI::InteractionParameterNotDefined( "parameter not defined" );
-  }
+    template< typename T >
+    inline RTI::ParameterHandle get_param_handle() {
+        throw RTI::InteractionParameterNotDefined("parameter not defined");
+    }
 
-  void add_values( boost::shared_ptr< RTI::ParameterHandleValuePairSet > ) {}
+    void add_values(boost::shared_ptr< RTI::ParameterHandleValuePairSet >) {}
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 template< typename A >
-struct param_inherit< A, param_empty_base > : A, param_empty_base
-{
-  template< typename T >
-  void init_handles( const std::string &class_name )
-  {
-    A::template init_handle< T >( class_name );
-  }
+struct param_inherit< A, param_empty_base > : A, param_empty_base {
 
-  void add_values( boost::shared_ptr< RTI::ParameterHandleValuePairSet> set_ptr )
-  {
-    A::add_value( set_ptr );
-  }
+    template< typename T >
+    void init_handles(const std::string &class_name) { A::template init_handle< T >(class_name); }
+
+    void add_values(boost::shared_ptr< RTI::ParameterHandleValuePairSet> set_ptr) { A::add_value(set_ptr); }
   
-  void recv_values( const RTI::ParameterHandleValuePairSet &ph_set )
-  {
-    A::recv_value( ph_set );
-  }
+    void recv_values(const RTI::ParameterHandleValuePairSet &ph_set) { A::recv_value(ph_set); }
 
-  static RTI::ULong count_params()
-  {
-    return 1;
-  }
+    static RTI::ULong count_params() { return 1; }
 
-  template< typename T >
-  inline typename T::value_type const &p_() const
-  {
-    return( static_cast< param_base< T > const & >( *this ).value );
-  }
+    template< typename T >
+    inline typename T::value_type const &p_() const { return (static_cast< param_base< T > const & >(*this).value); }
 
-  template< typename T >
-  inline typename T::value_type &p_()
-  {
-    return( static_cast< param_base< T > & >( *this ).value );
-  }
+    template< typename T >
+    inline typename T::value_type &p_() { return (static_cast< param_base< T > & >(*this).value); }
   
-  template< typename T >
-  inline typename T::value_type const &get_parameter() const
-  {
-    return( static_cast< param_base< T > const & >( *this ).value );
-  }
+    template< typename T >
+    inline typename T::value_type const &get_parameter() const {
+        return (static_cast< param_base< T > const & >(*this).value );
+    }
 
-  template< typename T >
-  inline typename T::value_type &get_parameter()
-  {
-    return( static_cast< param_base< T > & >( *this ).value );
-  }
+    template< typename T >
+    inline typename T::value_type &get_parameter() { return (static_cast< param_base< T > & >(*this).value); }
 
-  template< typename T >
-  inline RTI::ParameterHandle get_param_handle()
-  {
-    return( static_cast< param_base< T > & >( *this ).handle );
-  }
+    template< typename T >
+    inline RTI::ParameterHandle get_param_handle() { return(static_cast< param_base< T > & >(*this).handle); }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
  // A is an parameter and B is set of predecessor parameters.
 template< typename A, typename B >
-struct param_inherit : A, B
-{
-  template< typename T >
-  void init_handles( const std::string &class_name )
-  {
-    A::template init_handle< T >( class_name );
-    B::template init_handles< T >( class_name );
-  }
+struct param_inherit : A, B {
 
-  void add_values( boost::shared_ptr< RTI::ParameterHandleValuePairSet > set_ptr )
-  {
-    A::add_value( set_ptr );
-    B::add_values( set_ptr );
-  }
+    template< typename T >
+    void init_handles(const std::string &class_name) {
+        A::template init_handle< T >(class_name);
+        B::template init_handles< T >(class_name);
+    }
+
+    void add_values(boost::shared_ptr< RTI::ParameterHandleValuePairSet > set_ptr) {
+        A::add_value(set_ptr);
+        B::add_values(set_ptr);
+    }
   
-  void recv_values( const RTI::ParameterHandleValuePairSet &ph_set )
-  {
-    A::recv_value( ph_set );
-    B::recv_values( ph_set );
-  }
+    void recv_values(const RTI::ParameterHandleValuePairSet &ph_set) {
+        A::recv_value(ph_set);
+        B::recv_values(ph_set);
+    }
 
-  static RTI::ULong count_params()
-  {
-    unsigned int total = 1 + B::count_params();
-    return total;
-  }
+    static RTI::ULong count_params() {
+        unsigned int total = 1 + B::count_params();
+        return total;
+    }
 
-  template< typename T >
-  inline typename T::value_type const &p_() const
-  {
-    return( static_cast< param_base< T > const & >( *this ).value );
-  }
+    template< typename T >
+    inline typename T::value_type const &p_() const { return (static_cast< param_base< T > const & >(*this).value); }
 
-  template< typename T >
-  inline typename T::value_type &p_()
-  {
-    return( static_cast< param_base< T > & >( *this ).value );
-  }
+    template< typename T > inline typename T::value_type &p_() {
+        return (static_cast< param_base< T > & >(*this).value);
+    }
   
-  template< typename T >
-  inline typename T::value_type const &get_parameter() const
-  {
-    return( static_cast< param_base< T > const & >( *this ).value );
-  }
+    template< typename T >
+    inline typename T::value_type const &get_parameter() const {
+        return (static_cast< param_base< T > const & >(*this).value);
+    }
 
-  template< typename T >
-  inline typename T::value_type &get_parameter()
-  {
-    return( static_cast< param_base< T > & >( *this ).value );
-  }
+    template< typename T >
+    inline typename T::value_type &get_parameter() { return (static_cast< param_base< T > & >(*this).value); }
 
-  template< typename T >
-  inline RTI::ParameterHandle get_param_handle()
-  {
-    return( static_cast< param_base< T > & >( *this ).handle );
-  }
+    template< typename T >
+    inline RTI::ParameterHandle get_param_handle() { return (static_cast< param_base< T > & >(*this).handle); }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 }}
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 #endif
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
--- proto-x/include/protox/hla/recv_interaction.hpp
+++ proto-x/include/protox/hla/recv_interaction.hpp
@@ -1,88 +1,82 @@
-/*
-    Copyright (c) 2009 Jay Graham
+/**
+ *   Copyright (c) 2009 Jay Graham
+ *
+ *   Distributed under the MIT License (see accompanying file LICENSE_1_0_0.txt
+ *   or http://www.opensource.org/licenses/mit-license.php)
+ */
 
-    Distributed under the MIT License (see accompanying file LICENSE_1_0_0.txt
-    or http://www.opensource.org/licenses/mit-license.php)
-*/
-
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 #ifndef PROTOX_HLA_RECV_INTERACTION_HPP
 #define PROTOX_HLA_RECV_INTERACTION_HPP
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 #include <RTI.hh>
 
 #include <boost/mpl/empty_base.hpp>
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 namespace protox {
 namespace hla {
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 template< typename T >
-struct recv_interaction_base
-{
-  void (*handler) ( const T &, const RTI::FedTime *time, const char *tag );
+struct recv_interaction_base {
+    void (*handler) (const T &, const RTI::FedTime *time, const char *tag);
 
-  recv_interaction_base() : handler(0) {}
+    recv_interaction_base() : handler(0) {}
 
-  bool recv( RTI::InteractionClassHandle interaction_handle,
-             const RTI::ParameterHandleValuePairSet &params,
-             const RTI::FedTime *time,
-             const char *tag )
-  {
-    if (T::get_handle() == interaction_handle)
-    {
-      T new_interaction;
-      new_interaction.recv( params );
+    bool recv(      RTI::InteractionClassHandle       interaction_handle,
+              const RTI::ParameterHandleValuePairSet &params,
+              const RTI::FedTime                     *time,
+              const char                             *tag) {
 
-      if (handler != 0)
-      {
-        (*handler)( new_interaction, time, tag ); 
-      }
+        if (T::get_handle() == interaction_handle) {
+            T new_interaction;
+            new_interaction.recv(params);
 
-      return true;
+            if (handler != 0) {
+                (*handler)(new_interaction, time, tag);
+            }
+
+            return true;
+        }
+
+        return false;
     }
-
-    return false;
-  }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 // Forward declaration
 template< typename A, typename B > struct recv_interaction_inherit;
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 template< typename A >
-struct recv_interaction_inherit< A, boost::mpl::empty_base > : A, boost::mpl::empty_base
-{
-  template< typename T >
-  inline void set_handler( void (*h)( const T &, const RTI::FedTime *, const char *tag ) )
-  {
-    static_cast< recv_interaction_base< T > & >( *this ).handler = h;
-  }
-
-  void recv_interaction( RTI::InteractionClassHandle            interaction_handle,
-                         const RTI::ParameterHandleValuePairSet &params,
-                         const RTI::FedTime                     *time,
-                         const char                             *tag )
-  {
-    bool received = A::recv( interaction_handle, params, time, tag );
-
-    if (!received)
-    {
-      throw RTI::InteractionClassNotKnown( "" );
+struct recv_interaction_inherit< A, boost::mpl::empty_base > : A, boost::mpl::empty_base {
+    template< typename T >
+    inline void set_handler(void (*h)(const T &, const RTI::FedTime *, const char *tag)) {
+        static_cast< recv_interaction_base< T > & >(*this).handler = h;
     }
-  }
+
+    void recv_interaction(      RTI::InteractionClassHandle       interaction_handle,
+                          const RTI::ParameterHandleValuePairSet &params,
+                          const RTI::FedTime                     *time,
+                          const char                             *tag) {
+
+        bool received = A::recv(interaction_handle, params, time, tag);
+
+        if (!received) {
+            throw RTI::InteractionClassNotKnown("");
+        }
+    }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 /**
  * Defines \c recv_interaction functions.
@@ -90,38 +84,34 @@ struct recv_interaction_inherit< A, boost::mpl::empty_base > : A, boost::mpl::em
 
 // A is recv_interaction and B is set of predecessor recv_interactions.
 template< typename A, typename B >
-struct recv_interaction_inherit : A, B
-{
-  template< typename T >
-  inline void set_handler( void (*h)( const T &, const RTI::FedTime *, const char *tag ) )
-  {
-    static_cast< recv_interaction_base< T > & >( *this ).handler = h;
-  }
-
-  /**
-   * Forward interactions received via an HLA federate ambassador to this method to invoke
-   * any callbacks that have been registerd with this ambassador.
-   */
-  void recv_interaction( RTI::InteractionClassHandle             interaction_handle,
-                         const RTI::ParameterHandleValuePairSet &params,
-                         const RTI::FedTime                     *time,
-                         const char                             *tag )
-  {
-    if (A::recv( interaction_handle, params, time, tag ))
-    {
-      return;
+struct recv_interaction_inherit : A, B {
+    template< typename T >
+    inline void set_handler(void (*h)(const T &, const RTI::FedTime *, const char *tag)) {
+        static_cast< recv_interaction_base< T > & >(*this).handler = h;
     }
 
-    B::recv_interaction( interaction_handle, params, time, tag );
-  }
+    /**
+     * Forward interactions received via an HLA federate ambassador to this method to invoke any callbacks that have
+     * been registered with this ambassador.
+     */
+    void recv_interaction(      RTI::InteractionClassHandle       interaction_handle,
+                          const RTI::ParameterHandleValuePairSet &params,
+                          const RTI::FedTime                     *time,
+                          const char                             *tag) {
+        if (A::recv(interaction_handle, params, time, tag)) {
+            return;
+        }
+
+        B::recv_interaction(interaction_handle, params, time, tag);
+    }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 }}
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 #endif
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
