45089bec154e7c5dd3743e844dab4a4805fe57b3
code.trek.1 <code.trek.1@b8e2dd6e-3391-11de-8fef-1f5ae417a2df>
2012-03-02 02:38:48 +0000

    Unit tests refactoring

--- proto-x/include/protox/hla/remote_object.hpp
+++ proto-x/include/protox/hla/remote_object.hpp
@@ -5,12 +5,12 @@
     or http://www.opensource.org/licenses/mit-license.php)
 */
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 #ifndef PROTOX_HLA_REMOTE_OBJECT_HPP
 #define PROTOX_HLA_REMOTE_OBJECT_HPP
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 #include <map>
 
@@ -20,12 +20,12 @@
 
 #include <protox/hla/object_event_type.hpp>
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 namespace protox {
 namespace hla {
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 /**
  * Maintains a database of discovered objects of type \a T.
@@ -33,157 +33,127 @@ namespace hla {
  * \tparam T The type of managed object.
  */
 template< typename T >
-struct remote_object_base
-{
-  typedef T remote_object_type;
-  typedef std::map< RTI::ObjectHandle, remote_object_type > map_type;
+struct remote_object_base {
+    typedef T remote_object_type;
+    typedef std::map< RTI::ObjectHandle, remote_object_type > map_type;
 
-  /**
-   * Used to iterate over objects of type \a T.
-   */
-  typedef typename map_type::const_iterator const_it;
+    /**
+     * Used to iterate over objects of type \a T.
+     */
+    typedef typename map_type::const_iterator const_it;
 
-  map_type objects;
+    map_type objects;
 
-  void (*handler)
-    ( const remote_object_type &, object_event_type, const RTI::FedTime *, const char *tag );
+    void (*handler) (const remote_object_type &, object_event_type, const RTI::FedTime *, const char *tag);
 
-  remote_object_base() : handler(0) {}
+    remote_object_base() : handler(0) {}
 
-  bool add_object( RTI::ObjectClassHandle class_handle,
-                   RTI::ObjectHandle object_handle,
-                   const char *object_name )
-  {
-    if (remote_object_type::get_handle() != class_handle)
-    {
-      return false;
+    bool add_object(RTI::ObjectClassHandle class_handle, RTI::ObjectHandle object_handle, const char *object_name) {
+        if (remote_object_type::get_handle() != class_handle) {
+            return false;
+        }
+
+        objects[object_handle] = remote_object_type();
+
+        if (object_name != 0) {
+            objects[object_handle].set_obj_name(std::string(object_name));
+        }
+
+        if (handler != 0) {
+            (*handler)(objects[object_handle], protox::hla::OBJ_DISCOVERED, 0, 0);
+        }
+
+        return true;
     }
 
-    objects[ object_handle ] = remote_object_type();
+    bool delete_object(RTI::ObjectHandle object_handle) {
+        if (objects.find(object_handle) != objects.end()) {
+            if (handler != 0) {
+                (*handler)(objects[object_handle], protox::hla::OBJ_REMOVED, 0, 0);
+            }
+        }
 
-    if (object_name != 0)
-    {
-      objects[ object_handle ].set_obj_name( std::string( object_name ) );
+        return (objects.erase( object_handle ) == 1);
     }
 
-    if (handler != 0)
-    {
-      (*handler)( objects[ object_handle ], protox::hla::OBJ_DISCOVERED, 0, 0 );
+    bool reflect(      RTI::ObjectHandle                 object_handle,
+                 const RTI::AttributeHandleValuePairSet &attrs,
+                 const RTI::FedTime                     *time,
+                 const char                             *tag) {
+
+        typename map_type::iterator it = objects.find(object_handle);
+
+        if (it == objects.end()) {
+            return false;
+        }
+
+        it->second.reflect(attrs, time);
+
+        if (handler != 0) {
+            (*handler)(objects[object_handle], protox::hla::OBJ_REFLECTED, time, tag);
+        }
+
+        return true;
     }
-
-    return true;
-  }
-
-  bool delete_object( RTI::ObjectHandle object_handle )
-  {
-   if (objects.find( object_handle ) != objects.end())
-    {
-      if (handler != 0)
-      {
-        (*handler)( objects[ object_handle ], protox::hla::OBJ_REMOVED, 0, 0 );
-      }
-    }
-
-    return (objects.erase( object_handle ) == 1);
-  }
-
-  bool reflect( RTI::ObjectHandle object_handle,
-                const RTI::AttributeHandleValuePairSet &attrs,
-                const RTI::FedTime *time,
-                const char *tag )
-  {
-    typename map_type::iterator it = objects.find( object_handle );
-
-    if (it == objects.end())
-    {
-      return false;
-    }
-
-    it->second.reflect( attrs, time );
-
-    if (handler != 0)
-    {
-      (*handler)( objects[ object_handle ], protox::hla::OBJ_REFLECTED, time, tag );
-    }
-
-    return true;
-  }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 // Forward declaration
 template< typename A, typename B > struct remote_object_inherit;
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 template< typename A >
-struct remote_object_inherit< A, boost::mpl::empty_base > : A, boost::mpl::empty_base
-{
-  template< typename T >
-  inline void set_handler
-    ( void (*h)( const T &, object_event_type, const RTI::FedTime *, const char *tag ) )
-  {
-    static_cast< remote_object_base< T > & >( *this ).handler = h;
-  }
+struct remote_object_inherit< A, boost::mpl::empty_base > : A, boost::mpl::empty_base {
 
-  void discover_object( RTI::ObjectClassHandle class_handle,
-                        RTI::ObjectHandle object_handle,
-                        const char *object_name )
-  {
-    bool obj_added = A::add_object( class_handle, object_handle, object_name );
-
-    if (!obj_added)
-    {
-      throw RTI::ObjectClassNotKnown( "" );
+    template< typename T >
+    inline void set_handler(void (*h)(const T &, object_event_type, const RTI::FedTime *, const char *tag)) {
+        static_cast< remote_object_base< T > & >(*this).handler = h;
     }
-  }
 
-  void remove_object( RTI::ObjectHandle object_handle )
-  {
-    A::delete_object( object_handle );
-  }
+    void discover_object(      RTI::ObjectClassHandle class_handle,
+                               RTI::ObjectHandle      object_handle,
+                         const char                  *object_name) {
+        bool obj_added = A::add_object(class_handle, object_handle, object_name);
 
-  void reflect_object( RTI::ObjectHandle object_handle,
-                       const RTI::AttributeHandleValuePairSet &attrs,
-                       const RTI::FedTime *time,
-                       const char *tag )
-  {
-    const bool reflected = A::reflect( object_handle, attrs, time, tag );
-
-    if (!reflected)
-    {
-      throw RTI::ObjectNotKnown( "" );
+        if (!obj_added) {
+            throw RTI::ObjectClassNotKnown("");
+        }
     }
-  }
+
+    void remove_object(RTI::ObjectHandle object_handle) { A::delete_object(object_handle); }
+
+    void reflect_object(      RTI::ObjectHandle                 object_handle,
+                        const RTI::AttributeHandleValuePairSet &attrs,
+                        const RTI::FedTime                     *time,
+                        const char                             *tag) {
+        const bool reflected = A::reflect(object_handle, attrs, time, tag);
+
+        if (!reflected) {
+            throw RTI::ObjectNotKnown("");
+        }
+    }
 
 
-  template< typename T >
-  inline typename remote_object_base< T >::const_it begin() const
-  {
-    return (static_cast< remote_object_base< T > const & >( *this ).objects.begin());
-  }
+    template< typename T >
+    inline typename remote_object_base< T >::const_it begin() const {
+        return (static_cast< remote_object_base< T > const & >(*this).objects.begin());
+    }
 
-  template< typename T >
-  inline typename remote_object_base< T >::const_it end() const
-  {
-    return (static_cast< remote_object_base< T > const & >( *this ).objects.end());
-  }
+    template< typename T >
+    inline typename remote_object_base< T >::const_it end() const {
+        return (static_cast< remote_object_base< T > const & >(*this).objects.end());
+    }
 
-  template< typename T >
-  inline std::size_t size() const
-  {
-    return( static_cast< remote_object_base< T > const & >( *this ).objects.size() );
-  }
+    template< typename T >
+    inline std::size_t size() const { return (static_cast< remote_object_base< T > const & >(*this).objects.size()); }
 
-  template< typename T >
-  inline bool empty() const
-  {
-    return (static_cast< remote_object_base< T > const & >( *this ).objects.empty());
-  }
+    template< typename T >
+    inline bool empty() const { return (static_cast< remote_object_base< T > const & >(*this).objects.empty()); }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 /**
  * Defines \c remote_object functions.
@@ -275,12 +245,12 @@ struct remote_object_inherit : A, B
   }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 }}
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 #endif
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
