de5d6ee477ab33e214363f4c49959cd38d06253a
code.trek.1 <code.trek.1@b8e2dd6e-3391-11de-8fef-1f5ae417a2df>
2012-03-09 03:02:54 +0000

    unit test refactoring

--- proto-x/include/protox/hla_13/basic_data_encoders.hpp
+++ proto-x/include/protox/hla_13/basic_data_encoders.hpp
@@ -5,142 +5,127 @@
     or http://www.opensource.org/licenses/mit-license.php)
 */
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 #ifndef PROTOX_HLA_13_POLICY_BASIC_ENCODERS_HPP
 #define PROTOX_HLA_13_POLICY_BASIC_ENCODERS_HPP
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 #include <protox/hla_13/basic_data_representation_table.hpp>
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 namespace protox {
 namespace hla_13 {
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 // Basic type encoders
 template< typename S >
-inline void encode( S &s, char const c )
-{
-  s.push_back( c );
+inline void encode(S &s, char const c) {
+    s.push_back(c);
 }
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 template<
-  typename S,                // The destination byte buffer
-  std::size_t SIZE_IN_BYTES, // Size of value to be encoded in bytes
-  bool CONVERT_ENDIAN        // True, if endian conversion is required
+    typename S,                // The destination byte buffer
+    std::size_t SIZE_IN_BYTES, // Size of value to be encoded in bytes
+    bool CONVERT_ENDIAN        // True, if endian conversion is required
 > struct encode_basic;
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 template< typename S, bool CONVERT_ENDIAN >
-struct encode_basic< S, 1, CONVERT_ENDIAN >
-{
-  static inline void pack( S &s, HLA13octet::value_type const *value )
-  {
-    s.push_back( value[ 0 ] );
-  }
+struct encode_basic< S, 1, CONVERT_ENDIAN > {
+    static inline void pack(S &s, HLA13octet::value_type const *value) {
+        s.push_back(value[0]);
+    }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 template< typename S >
-struct encode_basic< S, 2, false >
-{
-  static inline void pack( S &s, HLA13octet::value_type const *value )
-  {
-    s.push_back( value[ 0 ] );
-    s.push_back( value[ 1 ] );
-  }
+struct encode_basic< S, 2, false > {
+    static inline void pack(S &s, HLA13octet::value_type const *value) {
+        s.push_back(value[0]);
+        s.push_back(value[1]);
+    }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 template< typename S >
-struct encode_basic< S, 4, false >
-{
-  static inline void pack( S &s, HLA13octet::value_type const *value )
-  {
-    s.push_back( value[ 0 ] );
-    s.push_back( value[ 1 ] );
-    s.push_back( value[ 2 ] );
-    s.push_back( value[ 3 ] );
-  }
+struct encode_basic< S, 4, false > {
+    static inline void pack(S &s, HLA13octet::value_type const *value) {
+        s.push_back(value[0]);
+        s.push_back(value[1]);
+        s.push_back(value[2]);
+        s.push_back(value[3]);
+    }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 template< typename S >
-struct encode_basic< S, 8, false >
-{
-  static inline void pack( S &s, HLA13octet::value_type const *value )
-  {
-    s.push_back( value[ 0 ] );
-    s.push_back( value[ 1 ] );
-    s.push_back( value[ 2 ] );
-    s.push_back( value[ 3 ] );
-    s.push_back( value[ 4 ] );
-    s.push_back( value[ 5 ] );
-    s.push_back( value[ 6 ] );
-    s.push_back( value[ 7 ] );
-  }
+struct encode_basic< S, 8, false > {
+    static inline void pack(S &s, HLA13octet::value_type const *value) {
+        s.push_back(value[0]);
+        s.push_back(value[1]);
+        s.push_back(value[2]);
+        s.push_back(value[3]);
+        s.push_back(value[4]);
+        s.push_back(value[5]);
+        s.push_back(value[6]);
+        s.push_back(value[7]);
+    }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 template< typename S >
-struct encode_basic< S, 2, true >
-{
-  static inline void pack( S &s, HLA13octet::value_type const *value )
-  {
-    s.push_back( value[ 1 ] );
-    s.push_back( value[ 0 ] );
-  }
+struct encode_basic< S, 2, true > {
+    static inline void pack(S &s, HLA13octet::value_type const *value) {
+        s.push_back(value[1]);
+        s.push_back(value[0]);
+    }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 template< typename S >
-struct encode_basic< S, 4, true >
-{
-  static inline void pack( S &s, HLA13octet::value_type const *value )
-  {
-    s.push_back( value[ 3 ] );
-    s.push_back( value[ 2 ] );
-    s.push_back( value[ 1 ] );
-    s.push_back( value[ 0 ] );
-  }
+struct encode_basic< S, 4, true > {
+    static inline void pack(S &s, HLA13octet::value_type const *value) {
+        s.push_back(value[3]);
+        s.push_back(value[2]);
+        s.push_back(value[1]);
+        s.push_back(value[0]);
+    }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 template< typename S >
-struct encode_basic< S, 8, true >
-{
-  static inline void pack( S &s, HLA13octet::value_type const *value )
-  {
-    s.push_back( value[ 7 ] );
-    s.push_back( value[ 6 ] );
-    s.push_back( value[ 5 ] );
-    s.push_back( value[ 4 ] );
-    s.push_back( value[ 3 ] );
-    s.push_back( value[ 2 ] );
-    s.push_back( value[ 1 ] );
-    s.push_back( value[ 0 ] );
-  }
+struct encode_basic< S, 8, true > {
+    static inline void pack(S &s, HLA13octet::value_type const *value) {
+        s.push_back(value[7]);
+        s.push_back(value[6]);
+        s.push_back(value[5]);
+        s.push_back(value[4]);
+        s.push_back(value[3]);
+        s.push_back(value[2]);
+        s.push_back(value[1]);
+        s.push_back(value[0]);
+    }
 };
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 }}
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
 
 #endif
 
-/**************************************************************************************************/
+/**********************************************************************************************************************/
